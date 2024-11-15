9d5063ac9213eed8a5eab6e4b458536caecb9590
hyun <apfhd0257@naver.com>
2024-11-15 14:48:59 +0900

    JDK 동적 프록시 한계 - 구체 클래스로 타입 캐스팅 불가

---
+++ src/test/java/com/example/demo/proxyvs/ProxyDITest.java
@@ -0,0 +1,31 @@
+package com.example.demo.proxyvs;
+
+import com.example.demo.member.MemberService;
+import com.example.demo.member.MemberServiceImpl;
+import com.example.demo.proxyvs.code.ProxyDIAspect;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Slf4j
+//@SpringBootTest(properties = {"spring.aop.proxy-target-class=false"}) // JDK 동적 프록시
+@SpringBootTest(properties = {"spring.aop.proxy-target-class=true"}) // JDK 동적 프록시
+@Import(ProxyDIAspect.class)
+public class ProxyDITest {
+
+    @Autowired
+    MemberService memberService;
+
+    @Autowired
+    MemberServiceImpl memberServiceImpl;
+
+    @Test
+    void go() {
+        log.info("memberService class={}", memberService.getClass());
+        log.info("memberServiceImpl class={}", memberServiceImpl.getClass());
+        memberServiceImpl.hello("hello");
+    }
+}

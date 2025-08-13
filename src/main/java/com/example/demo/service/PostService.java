624d9d8025cecb92cd82b4b9e95297683f5e12a0
hyun <apfhd0257@naver.com>
2025-08-13 21:23:38 +0900

    header 꺼내기

--- src/main/java/com/example/demo/service/PostService.java
+++ src/main/java/com/example/demo/service/PostService.java
@@ -1,9 +1,12 @@
 package com.example.demo.service;
 
 import com.example.demo.dto.PostResponse;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 import java.util.List;
 
@@ -17,8 +20,10 @@ public class PostService {
     }
 
     public PostResponse getPosts(Long postId) {
+        HttpServletRequest httpServletRequest = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
         PostResponse res = restClient.get()
                 .uri(url + "/" + postId)
+                .header("User-Agent", httpServletRequest.getHeader("User-Agent"))
                 .retrieve()
                 .body(PostResponse.class);
         return res;
